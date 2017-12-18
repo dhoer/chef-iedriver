925a136c8b21340d8603c2fcc11e5bb6ab76e9a9
Joshua Burns <joshuaburns@joshs-workbook.corp.auctiva.com>
2017-12-18 11:17:23 -0800

    fixed style issues for robucop

--- test/fixtures/cookbooks/iedriver_test/recipes/default.rb
+++ test/fixtures/cookbooks/iedriver_test/recipes/default.rb
@@ -7,8 +7,8 @@ node.default['selenium']['node']['capabilities'] = [
     browserName: 'internet explorer',
     maxInstances: 1,
     version: ie_version,
-    seleniumProtocol: 'WebDriver',
-  },
+    seleniumProtocol: 'WebDriver'
+  }
 ]
 # node.default['selenium']['node']['username'] = 'vagrant'
 # node.default['selenium']['node']['password'] = 'vagrant'
